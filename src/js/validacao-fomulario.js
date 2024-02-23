54f7a125c4b01d331e79bd16c36782014acd3420
Lucas Jones <jon3s@outlook.com>
2024-02-22 23:00:06 -0300

    Adicionado arquivos do projeto ao github

---
+++ src/js/validacao-fomulario.js
@@ -0,0 +1,26 @@
+const botaoEnviar = document.querySelector('#btEnviar')
+const camposObrigatorios = document.querySelectorAll('.campoObrigatorio')
+const textoCampoObrigatorio = document.querySelectorAll('.alertaCampoObrigatorio')
+
+botaoEnviar.addEventListener('click', ()=>{
+    verificarPreenchimento()
+})
+
+function verificarPreenchimento() {
+    for (let i = 0; i < 4; i++) {
+        console.log(camposObrigatorios[i])
+        textoCampoObrigatorio[i].style.opacity = 0
+
+        if (camposObrigatorios[i].value == "") {
+            camposObrigatorios[i].classList.add('AlertaEsteCampoEhObrigatorio')
+            camposObrigatorios[i].classList.remove('campoPreenchido')
+            textoCampoObrigatorio[i].style.opacity = 1
+            console.log('i')
+        } else {
+            camposObrigatorios[i].classList.add('campoPreenchido')
+            camposObrigatorios[i].classList.remove('AlertaEsteCampoEhObrigatorio')
+            textoCampoObrigatorio[i].style.opacity = 0
+        }
+    }
+}
+
